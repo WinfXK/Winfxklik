f1d59b46b9854f42587992d5db05d3585854977e
WinfxkAtLexy <by@winfxk.cn>
2025-08-08 08:42:45 +0800

    update

--- src/main/kotlin/cn/winfxk/libk/sql/hibernate/HibernateConfigurator.kt
+++ src/main/kotlin/cn/winfxk/libk/sql/hibernate/HibernateConfigurator.kt
@@ -57,36 +57,40 @@ class HibernateConfigurator(
     private val annotatedClassesToAdd = mutableSetOf<Class<*>>()
     private val log = Log(this.javaClass.simpleName)
     private var optionalProperties = Properties()
+    private var schemaManagementStrategy: String = "validate"
+    private var enableSecondLevelCache = false
 
     companion object {
-        /**
-         * 静态的默认配置。
-         */
         private val DEFAULT_PROPERTIES: Properties = Properties().apply {
-            // --- 默认使用 HikariCP 作为连接池提供者 ---
             put("hibernate.connection.provider_class", "org.hibernate.hikaricp.internal.HikariCPConnectionProvider")
-
-            // --- HikariCP 自身的推荐默认值 ---
-            put("hibernate.hikari.poolName", "WinfxkHikariPool") // 为连接池命名，便于监控
-            put("hibernate.hikari.maximumPoolSize", "20")       // 最大连接数
-            put("hibernate.hikari.minimumIdle", "5")            // 最小空闲连接数
-            put("hibernate.hikari.idleTimeout", "300000")       // 空闲连接最大存活时间 (5分钟)
-            put("hibernate.hikari.connectionTimeout", "20000")  // 获取连接的超时时间 (20秒)
-            put("hibernate.hikari.maxLifetime", "1800000")      // 连接最大生命周期 (30分钟)
+            put("hibernate.hikari.poolName", "WinfxkHikariPool")
+            put("hibernate.hikari.maximumPoolSize", "20")
+            put("hibernate.hikari.minimumIdle", "5")
+            put("hibernate.hikari.idleTimeout", "300000")
+            put("hibernate.hikari.connectionTimeout", "20000")
+            put("hibernate.hikari.maxLifetime", "1800000")
+            put("hibernate.show_sql", "true")
+            put("hibernate.format_sql", "true")
         }
     }
 
     /**
-     * 加载用户自定义的、可选的Hibernate配置。
-     * 此处传入的配置项会覆盖掉同名的默认配置。
+     * 设置数据库模式 (Schema) 管理策略。
      *
-     * @param properties 包含可选Hibernate属性的Properties对象。
+     * @param strategy 策略值，可选 "none", "validate", "update", "create", "create-drop"。
+     * 为了实现自动创建/更新，请使用 "update"。
      * @return 返回当前实例以支持链式调用。
      */
+    fun withSchemaManagement(strategy: String) = apply {
+        this.schemaManagementStrategy = strategy
+    }
+
     fun withOptionalProperties(properties: Properties) = apply {
         this.optionalProperties = properties
     }
-
+    fun withSecondLevelCache() = apply {
+        this.enableSecondLevelCache = true
+    }
     /**
      * 添加一个需要扫描实体类的包名。
      * @return 返回当前实例以支持链式调用。
@@ -113,11 +117,21 @@ class HibernateConfigurator(
         log.i("正在合并默认配置...")
         val finalProperties = DEFAULT_PROPERTIES.clone() as Properties
         finalProperties.putAll(optionalProperties)
-        finalProperties.put("hibernate.connection.url", this.url)
-        finalProperties.put("hibernate.connection.username", this.username)
-        finalProperties.put("hibernate.connection.password", this.password)
-        finalProperties.put("hibernate.connection.driver_class", this.driverClass)
-        finalProperties.put("hibernate.dialect", this.dialect)
+        finalProperties["hibernate.connection.url"] = this.url
+        finalProperties["hibernate.connection.username"] = this.username
+        finalProperties["hibernate.connection.password"] = this.password
+        finalProperties["hibernate.connection.driver_class"] = this.driverClass
+        finalProperties["hibernate.dialect"] = this.dialect
+        finalProperties["hibernate.hbm2ddl.auto"] = this.schemaManagementStrategy
+        log.i("数据库模式管理策略设置为: ${this.schemaManagementStrategy}")
+        if (enableSecondLevelCache) {
+            log.i("正在启用并配置二级缓存 (L2 Cache)...")
+            finalProperties["hibernate.cache.use_second_level_cache"] = "true"
+            finalProperties["hibernate.cache.use_query_cache"] = "true"
+            finalProperties["hibernate.cache.region.factory_class"] = "org.hibernate.cache.jcache.JCacheRegionFactory"
+            finalProperties["hibernate.javax.cache.provider"] = "com.github.benmanes.caffeine.jcache.CachingProvider"
+            log.i("二级缓存已配置，使用 Caffeine 作为实现。")
+        }
         serviceRegistryBuilder.applySettings(finalProperties)
         log.i("配置合并完成。")
         log.i("开始构建 ServiceRegistry...")
@@ -139,14 +153,12 @@ class HibernateConfigurator(
                 }
             }
             log.i("元数据构建完成，正在构建 SessionFactory...")
-            metadataSources.buildMetadata().buildSessionFactory()
+            val sessionFactory = metadataSources.buildMetadata().buildSessionFactory()
+            log.i("SessionFactory 构建成功！")
+            sessionFactory
         } catch (e: Exception) {
             log.e("构建 SessionFactory 失败：${e.message}", e)
-            try {
-                registry.close()
-            } catch (e2: Exception) {
-                log.e("关闭 ServiceRegistry 失败", e2)
-            }
+            StandardServiceRegistryBuilder.destroy(registry)
             throw e
         }
     }
